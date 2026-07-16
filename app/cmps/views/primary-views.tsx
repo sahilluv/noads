cc11ff930bdbf5eec03e1f10ba8b157549a2129b
nikhilchari <nikhilchari2005@gmail.com>
2026-07-16 11:14:37 +0530

    fix: render price overlay and dashboard in primary views

--- app/cmps/views/primary-views.tsx
+++ app/cmps/views/primary-views.tsx
@@ -1,11 +1,15 @@
+import { Dashboard } from '../layout/dashboard'
 import { About } from './about'
 import { BuyModal } from './buy-modal'
 import { HotkeysView } from './hotkeys'
 import { LowFpsAlert } from './low-fps-alert'
+import { PriceOverlay } from './price-overlay/price-overlay'
 import { Settings } from './settings'
 
 const PrimaryViews = () => (
   <>
+    <PriceOverlay />
+    <Dashboard />
     <Settings />
     <About />
     <LowFpsAlert />
